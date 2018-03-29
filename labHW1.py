5d1af11a33a3364e096d592e6a321dc9d96a95b6
Yoyo <yoyo840401tw@gmail.com>
2018-03-30 05:39:07 +0800

    finished 3 questions

--- labHW1.py
+++ labHW1.py
@@ -11,6 +11,7 @@ df_T2 = pd.read_csv("105Taoyuan_table2.csv", encoding="big5")
 df_T3 = pd.read_csv("105Taoyuan_target_variable.csv", encoding="big5")
 
 
+
 ''' merge 3 Jongli tables in 1 '''
 res_Jongli = pd.merge(df_J1, df_J2, on=['date','location','time'])          # merge Jongli table #1 and #2
 res_Jongli = res_Jongli.drop(columns=res_Jongli.columns[0])                 # drop the Unnamed column
@@ -21,6 +22,7 @@ res_Jongli = res_Jongli.drop(columns=res_Jongli.columns[20])                # dr
 #res_Jongli.to_csv("test.csv", encoding="big5")
 
 
+
 ''' merge 3 Taoyuan tables in 1 '''
 res_Taoyuan = pd.merge(df_T1, df_T2, on=['date','location','time'])         # merge Taoyuan table #1 and #2
 res_Taoyuan = res_Taoyuan.drop(columns=res_Taoyuan.columns[0])              # drop the Unnamed column
@@ -29,6 +31,7 @@ res_Taoyuan = res_Taoyuan.drop(columns=res_Taoyuan.columns[12])             # dr
 res_Taoyuan = pd.merge(res_Taoyuan, df_T3, on=['date','location','time'])   # the final merge of Taoyuan table
 res_Taoyuan = res_Taoyuan.drop(columns=res_Taoyuan.columns[21])             # drop the Unnamed column
 #res_Taoyuan.to_csv("test2.csv", encoding="big5")
+
 
 
 ''' merge Jongli and Taoyuan '''
@@ -42,29 +45,38 @@ result.replace('\*|#|-|[A-Z]|[a-z]', 0, regex=True, inplace=True)           # cl
 #result.replace('NR', 0, inplace=True)
 result.fillna(0, inplace=True)
 
-result.to_csv("105Jongli_and_Taoyuan.csv", encoding="big5")
+result.reset_index(inplace=True, drop=True)                                 # reindex
+
+result.to_csv("105Jongli_and_Taoyuan.csv", encoding="big5")                 # the final merged table of Jongli and Taoyuan
+
 
 
 ''' (1) '''
-result['SO2'] = result.SO2.astype(float)
+result['SO2'] = result.SO2.astype(float)                                    # change the type to float
 #print(result.dtypes)
-ans1 = result['SO2'].max()
+ans1 = result['SO2'].max()                                                  # find max
 print("\n(1) The maximum value of SO2 is : ", ans1)
 
 
+
 ''' (2) '''
-result['CO'] = result.CO.astype(float)
-ans2 = result.loc[result.location=='中壢','CO'].mean()
+result['CO'] = result.CO.astype(float)                                      # change the type to float
+ans2 = result.loc[result.location=='中壢','CO'].mean()                      # calculate the average
 print("\n(2) The average value of CO in Jongli is :", ans2)
 
 
+
 ''' (3) '''
-result.rename(columns={'PM2.5':'PM25'}, inplace=True)
+result.rename(columns={'PM2.5':'PM25'}, inplace=True)                       # change the type to float
 result['PM25'] = result.PM25.astype(float)
 result.rename(columns={'PM25':'PM2.5'}, inplace=True)
-ans3_1 = result['date'][result['PM2.5'].idxmax()]
-ans3_2 = result['location'][result['PM2.5'].idxmax()]
 
-#print(result['PM2.5'].idxmax())
-print(ans3_1)
-print(ans3_2)
+ans3_1 = result['date'][result['PM2.5'].idxmax()]                           # date of maximum PM2.5
+ans3_2 = result['location'][result['PM2.5'].idxmax()]                       # location of maximum PM2.5
+
+print("\n(3) The maximum value of PM2.5 is in : ", ans3_1, ",", ans3_2)
+
+
+
+''' (4) '''
+print(result['date'].month)
